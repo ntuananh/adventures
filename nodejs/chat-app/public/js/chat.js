01e26a3a851fbacefc8163f4a3063d4376618c28
Anh Nguyen <tuan.nguyen@directenergy.com>
2019-05-31 15:50:39 -0500

    Init chat app

---
+++ nodejs/chat-app/public/js/chat.js
@@ -0,0 +1,72 @@
+const socket = io()
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+socket.on('message', (message) => {
+
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (locationMessage) => {
+
+    const html = Mustache.render(locationTemplate, {
+        url: locationMessage.url,
+        createdAt: moment(locationMessage.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
+})
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    $locationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude
+        }, () => {
+            $locationButton.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
+})
+
+socket.emit('join', { username, room })
